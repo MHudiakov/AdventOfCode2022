b04c1e544b6fb79dee3262a2894d04d2f452bb87
Maksim Khudiakov <mkhudiakov@travix.com>
2022-12-13 12:26:06 +0100

    Day 13 - Distress Signal

---
+++ 13 - Distress Signal/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿var input = (from line in File.ReadAllLines(@"c:\aoc/1.txt")
+    where !string.IsNullOrEmpty(line)
+    select ParseLine(line[1..]).list).ToList();
+
+int indicesSum = 0;
+for (int i = 0; i < input.Count; i+=2)
+{
+    var left = input[i];
+    var right = input[i + 1];
+    var rightOrder = CheckOrder(left, right);
+
+    if (rightOrder.result)
+    {
+        indicesSum += i / 2 + 1;
+    }
+}
+
+Console.WriteLine(indicesSum);
+Console.ReadLine();
+
+(bool result, bool goFurther) CheckOrder(List<object> left, List<object> right)
+{
+    for (int i = 0; i < left.Count; i++)
+    {
+        if (right.Count < i + 1)
+        {
+            return (false, false);
+        }
+
+        var lItem = left[i];
+        var rItem = right[i];
+
+        if (lItem is int && rItem is int)
+        {
+            if ((int)lItem < (int)rItem)
+            {
+                return (true, false);
+            }
+
+            if ((int)lItem > (int)rItem)
+            {
+                return (false, false);
+            }
+        }
+        else
+        {
+            var l = lItem is int ? new List<object> { (int)lItem } : (List<object>)lItem;
+            var r = rItem is int ? new List<object> { (int)rItem } : (List<object>)rItem;
+            var check = CheckOrder(l, r);
+
+            if (!check.goFurther)
+            {
+                return check;
+            }
+        }
+    }
+
+    if (right.Count > left.Count)
+    {
+        return (true, false);
+    }
+
+    return (false, true);
+}
+
+(List<object> list, int offset) ParseLine(string line)
+{
+    var result = new List<object>();
+    string digit = string.Empty;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        var symbol = line[i];
+
+        switch (line[i])
+        {
+            case var c when char.IsDigit(c):
+                digit += symbol;
+
+                if (!char.IsDigit(line[i + 1]))
+                {
+                    result.Add(int.Parse(digit));
+                    digit = string.Empty;
+                }
+
+                break;
+            case '[':
+                var parsedLine = ParseLine(line[(i + 1)..]);
+                result.Add(parsedLine.list);
+                i += parsedLine.offset + 1;
+                break;
+
+            case ']':
+                return (result, i);
+        }
+    }
+
+    return (result, 0);
+}
---
+++ 13 - Distress Signal/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,120 @@
+﻿var input = (from line in File.ReadAllLines(@"c:\aoc/1.txt")
+    where !string.IsNullOrEmpty(line)
+    select ParseLine(line[1..]).list).ToList();
+
+var dividerPacket1 = new List<object> { new List<object> { 2 } };
+var dividerPacket2 = new List<object> { new List<object> { 6 } };
+input.Add(dividerPacket1);
+input.Add(dividerPacket2);
+
+var orderedList = new List<List<object>>();
+foreach (var package in input)
+{
+    if (!orderedList.Any())
+    {
+        orderedList.Add(package);
+        continue;
+    }
+
+    for (int i = orderedList.Count - 1; i >= 0; i--)
+    {
+        if (CheckOrder(package, orderedList[i]).result)
+        {
+            if (i == 0)
+            {
+                orderedList.Insert(0, package);
+                break;
+            }
+        }
+        else
+        {
+            orderedList.Insert(i+1, package);
+            break;
+        }
+    }
+}
+
+var decoderKey = (orderedList.IndexOf(dividerPacket1) + 1) * (orderedList.IndexOf(dividerPacket2) + 1);
+
+Console.WriteLine(decoderKey);
+Console.ReadLine();
+
+(bool result, bool goFurther) CheckOrder(List<object> left, List<object> right)
+{
+    for (int i = 0; i < left.Count; i++)
+    {
+        if (right.Count < i + 1)
+        {
+            return (false, false);
+        }
+
+        var lItem = left[i];
+        var rItem = right[i];
+
+        if (lItem is int && rItem is int)
+        {
+            if ((int)lItem < (int)rItem)
+            {
+                return (true, false);
+            }
+
+            if ((int)lItem > (int)rItem)
+            {
+                return (false, false);
+            }
+        }
+        else
+        {
+            var l = lItem is int ? new List<object> { (int)lItem } : (List<object>)lItem;
+            var r = rItem is int ? new List<object> { (int)rItem } : (List<object>)rItem;
+            var check = CheckOrder(l, r);
+
+            if (!check.goFurther)
+            {
+                return check;
+            }
+        }
+    }
+
+    if (right.Count > left.Count)
+    {
+        return (true, false);
+    }
+
+    return (false, true);
+}
+
+(List<object> list, int offset) ParseLine(string line)
+{
+    var result = new List<object>();
+    string digit = string.Empty;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        var symbol = line[i];
+
+        switch (line[i])
+        {
+            case var c when char.IsDigit(c):
+                digit += symbol;
+
+                if (!char.IsDigit(line[i + 1]))
+                {
+                    result.Add(int.Parse(digit));
+                    digit = string.Empty;
+                }
+
+                break;
+            case '[':
+                var parsedLine = ParseLine(line[(i + 1)..]);
+                result.Add(parsedLine.list);
+                i += parsedLine.offset + 1;
+                break;
+
+            case ']':
+                return (result, i);
+        }
+    }
+
+    return (result, 0);
+}
