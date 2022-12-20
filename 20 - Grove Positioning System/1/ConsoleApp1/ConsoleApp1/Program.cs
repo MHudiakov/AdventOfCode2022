91504176bc8127eb366e03c35b65478d81d933dc
Maksim Khudiakov <mkhudiakov@travix.com>
2022-12-20 12:25:38 +0100

    20 - Grove Positioning System

---
+++ 20 - Grove Positioning System/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿var numbers = ReadInput();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    for (int j = 0; j < numbers.Count; j++)
+    {
+        if (!numbers[j].moved)
+        {
+            var value = numbers[j].value;
+            numbers.RemoveAt(j);
+
+            var insertPosition = 0;
+            if (j + value > 0)
+            {
+                insertPosition = (j + value) % numbers.Count;
+            }
+            else
+            {
+                insertPosition = numbers.Count + (j + value) % numbers.Count;
+            }
+
+            numbers.Insert(insertPosition, (value, true));
+            break;
+        }
+    }
+}
+
+var c1 = numbers[(1000 % numbers.Count + numbers.IndexOf((0, true))) % numbers.Count].value;
+var c2 = numbers[(2000 % numbers.Count + numbers.IndexOf((0, true))) % numbers.Count].value;
+var c3 = numbers[(3000 % numbers.Count + numbers.IndexOf((0, true))) % numbers.Count].value;
+
+int groveCoordinatesSum = c1 + c2 + c3;
+
+Console.WriteLine(groveCoordinatesSum);
+Console.ReadLine();
+
+List<(int value, bool moved)> ReadInput()
+{
+    return File.ReadAllLines(@"c:\aoc/1.txt")
+        .Select(line => (int.Parse(line), false)).ToList();
+}
---
+++ 20 - Grove Positioning System/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿var numbers = ReadInput();
+const long decryptionKey = 811589153;
+const int numberOfMixes = 10;
+
+for (int k = 0; k < numberOfMixes; k++)
+{
+    for (int j = 0; j < numbers.Count; j++)
+    {
+        var number = numbers.First(n => n.position == j);
+        var currentPosition = numbers.IndexOf(number);
+        numbers.RemoveAt(currentPosition);
+
+        var insertPosition = (currentPosition + number.value > 0)
+            ? (currentPosition + number.value) % numbers.Count
+            : numbers.Count + (currentPosition + number.value) % numbers.Count;
+
+        numbers.Insert((int)insertPosition, number);
+    }
+}
+
+int indexOfZero = numbers.FindIndex(n => n.value == 0);
+var c1 = numbers[(1000 % numbers.Count + indexOfZero) % numbers.Count].value;
+var c2 = numbers[(2000 % numbers.Count + indexOfZero) % numbers.Count].value;
+var c3 = numbers[(3000 % numbers.Count + indexOfZero) % numbers.Count].value;
+
+long groveCoordinatesSum = c1 + c2 + c3;
+
+Console.WriteLine(groveCoordinatesSum);
+
+List<(long value, int position)> ReadInput()
+{
+    int position = 0;
+    var input = new List<(long value, int position)>();
+    foreach (var line in File.ReadAllLines(@"c:\aoc/1.txt"))
+    {
+        input.Add((long.Parse(line) * decryptionKey, position));
+        position++;
+    }
+
+    return input;
+}
